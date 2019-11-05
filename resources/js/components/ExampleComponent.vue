99d7cc6e45a455f32c4061a4f73f4660c56dece4
Jorge David <jorgedpg10@gmail.com>
2019-11-05 14:29:32 -0500

    primer commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        Hola
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        created: function(){
+
+            var currentUrl = window.location.pathname;
+            
+            console.log(currentUrl);
+
+
+        }
+    }
+</script>
