c15177f17984acc14f671c80ed6c458ccb0ec27d
Jorge David <jorgedpg10@gmail.com>
2019-11-15 15:32:45 -0500

    termina vista soporte

--- resources/js/components/SoporteTecnico.vue
+++ resources/js/components/SoporteTecnico.vue
@@ -7,7 +7,12 @@
                         <div class="grid-content"></div>
                     </el-col>
                     <el-col :span="18">
-                        <div class="grid-content "><img class="img-mostrar" src="/images/soporte-tecnico.png">
+                        <div class="grid-content ">
+                            <img class="img-mostrar" src="/images/soporte-tecnico.png">
+                        </div>
+                        <div class="grid-content instrucciones" >
+                            <h5 class="titulo-soporte">Soporte técnico</h5>
+                            <p>Si tiene un problema con la aplicación, por favor no dude en enviar su novedad a nuestro personal de soporte.</p>
                         </div>
                     </el-col>
                     <el-col :span="3">
@@ -17,13 +22,10 @@
 
 
                 <el-col :span="24">
-                    <el-button class="boton-usuario" type="primary" round @click="dialogVisible = true">Decargar
-                        términos
-                        de servicio y asistencia
+                    <el-button class="boton-usuario" type="primary" round @click="">
+                        Enviar correo
                     </el-button>
-                    <el-button class="boton-usuario" type="primary" round @click="dialogVis = true">
-                        Solicitar factura electrónica
-                    </el-button>
+
 
                 </el-col>
 
@@ -38,6 +40,14 @@
 </script>
 
 <style scoped>
+    .titulo-soporte{
+        font-weight: bold;
+    }
+    .instrucciones{
+        text-align: left;
+        margin-top: 10px;
+        margin-left: 10px;
+    }
 
     .img-mostrar {
         width: 100%;
@@ -52,10 +62,13 @@
         width: 200px;
     }
 
+    .el-button >>> span {
+        white-space: initial;
+    }
+
     .el-main {
 
         text-align: center;
-        line-height: 80px;
     }
 
     body > .el-container {
